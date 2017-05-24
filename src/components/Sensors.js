c0f253177ba3bdc8f6e74f9d31d1e0a1f19d2347
kevin2106 <keviee0007@gmail.com>
2017-05-24 12:07:24 +0200

    Added: Recharts and changed sensor page layout

--- src/components/Sensors.js
+++ src/components/Sensors.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Nav from './Nav';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { getSensorTemps } from '../utils/ApiService';
+var _ = require('lodash');
 
 class Sensors extends Component {
 
@@ -30,19 +32,31 @@ class Sensors extends Component {
         <h3 className="text-center">Sensor Data</h3>
         <hr/>
 
+
+
         { sensors.map((sensor, index) => (
              <div className="col-sm-6" key={index}>
+             <LineChart width={400} height={400} data={sensor.Readings}>
+              <Line type="monotone" dataKey="Value" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" />
+              <XAxis dataKey="Timestamp"  />
+              <YAxis />
+              <Tooltip />
+             </LineChart>
                <div className="panel panel-primary">
                  <div className="panel-heading">
-                   <h3 className="panel-title"> <span className="btn">#{ sensor.id }</span></h3>
+                   <h3 className="panel-title"> <span className="btn">Name: { sensor.Name }</span><span className="pull-right btn">Type: {sensor.Type}</span></h3>
                  </div>
                  <div className="panel-body">
-                   <p> { sensor.temperature  } </p>
+                    <p> Current {sensor.Type}: { sensor.Readings[0].Value} {sensor.Unit}</p>
+                    <p> Active: {sensor.isActive} </p>
+                    <button type="button" className="btn btn-default" aria-label="Right Align">
+                        <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    </button>
                  </div>
                </div>
              </div>
          ))}
-
 
         <div className="col-sm-12">
           <div className="jumbotron text-center">
