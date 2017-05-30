b560c599eb8e1f63e8ece4620d2c03623ddb8492
kevin2106 <keviee0007@gmail.com>
2017-05-30 11:53:28 +0200

    ADDED: Sensor add option

--- src/components/Sensors.js
+++ src/components/Sensors.js
@@ -3,13 +3,18 @@ import { Link } from 'react-router';
 import Nav from './Nav';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import { getSensorTemps } from '../utils/ApiService';
+import SensorAddModal  from './SensorAddModal';
 var _ = require('lodash');
 
 class Sensors extends Component {
 
   constructor() {
     super();
-    this.state = { sensors: [] };
+    this.state =
+    {
+      sensors: [],
+      showGraphs: true
+    };
   }
 
   getSensorTemps() {
@@ -24,25 +29,31 @@ class Sensors extends Component {
 
   render() {
 
-    const { sensors } = this.state;
+    const { sensors, showGraph } = this.state;
+
+    const showHideGraph = {
+      'display' : this.state.showGraph ? 'block' : 'none'
+    };
+
+    const toggleGraph = () => {
+      this.setState(prevState => ({
+      showGraph: !prevState.showGraph
+    }));
+    console.log(this.state.showGraph)
+    };
 
     return (
       <div>
         <Nav />
         <h3 className="text-center">Sensor Data</h3>
+        <button type ="button" className="btn btn-default" aria-label="Right Align" onClick={toggleGraph}>
+            <i className="fa fa-line-chart" aria-hidden="true"/>
+        </button>
+        <SensorAddModal/>
         <hr/>
-
-
 
         { sensors.map((sensor, index) => (
              <div className="col-sm-6" key={index}>
-             <LineChart width={400} height={400} data={sensor.Readings}>
-              <Line type="monotone" dataKey="Value" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" />
-              <XAxis dataKey="Timestamp"  />
-              <YAxis />
-              <Tooltip />
-             </LineChart>
                <div className="panel panel-primary">
                  <div className="panel-heading">
                    <h3 className="panel-title"> <span className="btn">Name: { sensor.Name }</span><span className="pull-right btn">Type: {sensor.Type}</span></h3>
@@ -50,9 +61,19 @@ class Sensors extends Component {
                  <div className="panel-body">
                     <p> Current {sensor.Type}: { sensor.Readings[0].Value} {sensor.Unit}</p>
                     <p> Active: {sensor.isActive} </p>
+                    <div style={showHideGraph}>
+                    <LineChart width={400} height={400} data={sensor.Readings}>
+                     <Line type="monotone" dataKey="Value" stroke="#8884d8" />
+                     <CartesianGrid stroke="#ccc" />
+                     <XAxis dataKey="Timestamp"  />
+                     <YAxis />
+                     <Tooltip />
+                    </LineChart>
+                    </div>
                     <button type="button" className="btn btn-default" aria-label="Right Align">
                         <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                     </button>
+
                  </div>
                </div>
              </div>
--- src/utils/ApiService.js
+++ src/utils/ApiService.js
@@ -4,13 +4,21 @@ import { getAccessToken } from './AuthService';
 const BASE_URL = 'http://localhost:3333';
 
 export {getSensorTemps};
+var header = {
+  headers: { Authorization: `Bearer ${getAccessToken()}` }
+};
 
 function getSensorTemps() {
   const url = `${BASE_URL}/api/sensors`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+  return axios.get(url, header).then(response => response.data);
 }
 
 function getSensorItems(){
-  const url = `${BASE_URL}/api/sensors`
-  return axios.get(url, { headers: { Authorization: `Bearer ${getAccessToken()}` }}).then(response => response.data);
+  const url = `${BASE_URL}/api/sensors`;
+  return axios.get(url, header).then(response => response.data);
+}
+
+function postNewSensor(data){
+  const url = `${BASE_URL}/api/Sensor`;
+  return axios.post(url, data, header).then(response => response.data);
 }
